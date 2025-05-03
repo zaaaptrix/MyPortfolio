d4a1344cd6c3763f16eb60b75d772e71a71ad530
Kushal <gurjarkushal.18@gmail.com>
2025-05-03 14:59:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Simple Scroll to Section
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+}
